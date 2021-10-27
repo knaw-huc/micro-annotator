3660202b0732daa21ce34ca5801d0d5a449103d4
basl <basleenknegt@gmail.com>
2021-10-27 11:02:53 +0200

    Add ANNO_STORE_PATH, check env vars are set in init.sh

--- init/init.sh
+++ init/init.sh
@@ -5,7 +5,12 @@ set -e
 cd /usr/src/app/textrepo
 pip install -r requirements.txt
 
-# Expects TEXTREPO_HOST and TEXT_STORE_PATH:
+if [[ ! -v TEXTREPO_HOST ]]; then
+    echo "TEXTREPO_HOST is not set"
+fi
+if [[ ! -v TEXT_STORE_PATH ]]; then
+    echo "TEXT_STORE_PATH is not set"
+fi
 VERSION_ID=$(python ./import-text-store.py)
 
 echo "VERSION_ID: [${VERSION_ID}]"
@@ -13,6 +18,11 @@ echo "VERSION_ID: [${VERSION_ID}]"
 cd /usr/src/app
 pip install -r requirements.txt
 pip install .
-python ./scripts/convert_to_web_annotations.py data/1728-06-19-annotationstore.json
+
+if [[ ! -v ANNO_STORE_PATH ]]; then
+    echo "ANNO_STORE_PATH is not set"
+fi
+python ./scripts/convert_to_web_annotations.py $ANNO_STORE_PATH
+
 python ./scripts/export_to_elucidate.py -e http://elucidate:8080/annotation -c "$VERSION_ID"
 
